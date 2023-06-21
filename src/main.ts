997039308aa4f05df68da1e168cb47d68b6163e1
Errortime <larb26656@gmail.com>
2023-06-21 20:24:10 +0700

    Feat (add app info)

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { AppModule } from './app.module';
 import { CustomProxyMiddleware } from './custom-proxy-middleware';
 
 
+function printAppInfo() {
+  const blueColorCode = '\u001b[34m';
+  const orangeColorCode = '\u001b[38;5;208m';
+  const resetColorCode = '\u001b[0m';
+  const version = process.env.npm_package_version;
+  
+  console.log(`${blueColorCode}() ${resetColorCode}Postal proxy ${orangeColorCode}()${resetColorCode} v.${version}`);  
+}
 
 async function bootstrap() {
+  printAppInfo();
   // create custom proxy and assign to global
   global.coreProxy = new CustomProxyMiddleware('proxy.json');
 
